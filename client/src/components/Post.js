9051f53c296c1809c5f861541fe616206398c71a
Carlos Baez <carlosbaez@Green-Planet.local>
2020-05-11 01:48:55 -0400

    corrected the spelling in Post.js

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,7 +4,7 @@ const Post = props => {
     return (
         <div>
             <h3>Title: {props.title}</h3>
-            <h4>{props.content}</h4>
+            <h4>Content: {props.content}</h4>
             <p>Created: {props.created_at}</p>
             <hr />
         </div>
